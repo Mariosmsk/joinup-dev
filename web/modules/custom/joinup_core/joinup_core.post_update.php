1a96d2377a254ae8478806b840364cd70e6992a5
Ilias Dimopoulos <idimopoulos@hotmail.com>
2020-08-20 13:45:17 +0300

    ISAICP-5939: Provide an upgrade path for the existing entities as well.

--- web/modules/custom/joinup_core/joinup_core.post_update.php
+++ web/modules/custom/joinup_core/joinup_core.post_update.php
@@ -7,6 +7,9 @@
 
 declare(strict_types = 1);
 
+use Drupal\og\Entity\OgMembership;
+use Drupal\og\Entity\OgRole;
+
 /**
  * Update the values of current collections new member role.
  */
@@ -42,4 +45,37 @@ QUERY;
   }
 
   \Drupal::entityTypeManager()->getStorage('rdf_entity')->resetCache($ids);
+}
+
+/**
+ * Update memberships related to the new role.
+ */
+function joinup_core_post_update_0106403(&$sandbox) {
+  if (empty($sandbox['membership_ids'])) {
+    $collection_ids = \Drupal::entityTypeManager()->getStorage('rdf_entity')->getQuery()
+      ->condition('rid', 'collection')
+      ->condition('field_ar_new_member_role', 'rdf_entity-collection-author')
+      ->execute();
+
+    $sandbox['membership_ids'] = \Drupal::entityTypeManager()->getStorage('og_membership')->getQuery()
+      ->condition('entity_type', 'rdf_entity')
+      ->condition('entity_id', $collection_ids, 'IN')
+      ->execute();
+    $sandbox['progress'] = 0;
+    $sandbox['total'] = count($sandbox['membership_ids']);
+  }
+
+  $role = OgRole::load('rdf_entity-collection-author');
+  $membership_ids = array_splice($sandbox['membership_ids'], 0, 50);
+
+  /** @var \Drupal\og\OgMembershipInterface $membership */
+  foreach (OgMembership::loadMultiple($membership_ids) as $membership) {
+    $membership->addRole($role);
+    $membership->skip_notification = TRUE;
+    $membership->save();
+    $sandbox['progress']++;
+  }
+
+  $sandbox['#finished'] = (float) $sandbox['progress'] / (float) $sandbox['total'];
+  return "Processed {$sandbox['progress']} out of {$sandbox['total']} memberships.";
 }
